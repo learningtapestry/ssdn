0b19c8a4c8b903eb6dd4a64d3ebb723ce6cb6304
Abraham Sánchez <abraham@twintags.com>
2019-02-20 20:25:49 +0100

    Set up default application logger using 'Pino'

---
+++ src/logger.ts
@@ -0,0 +1,12 @@
+/**
+ * logger.ts: Main application logger, tailored to different environments
+ */
+
+import Pino from "pino";
+
+const logger = Pino({
+    level: process.env.NUCLEUS_LOG_LEVEL || "info",
+    prettyPrint: {colorize: true, translateTime: true},
+});
+
+export default logger;
