0d997e2e7e51975bab3b1e7cc034057d150e25e6
Abraham Sánchez <abraham@twintags.com>
2019-02-27 18:20:30 +0100

    Add service to handle event processing

---
+++ src/functions/process-event/event-service.test.ts
@@ -0,0 +1,44 @@
+import sinon from "sinon";
+import nucleusEvent from "../../../test-support/data-samples/nucleus-event.json";
+import EventService from "./event-service";
+
+describe("EventService", () => {
+    describe("process", () => {
+        const validator = {errors: sinon.stub().returns([]), validate: sinon.stub()};
+        const inMemoryRepository = {store: sinon.stub()};
+
+        beforeEach(() => {
+            validator.validate.returns(true);
+            inMemoryRepository.store.returnsArg(0);
+        });
+
+        it("stores the content when validation passes", async () => {
+            const results = await EventService.process(nucleusEvent, validator, inMemoryRepository);
+
+            expect(results).toHaveProperty("message", "Event has been processed successfully");
+            expect(results).not.toHaveProperty("errors");
+        });
+
+        it("returns validation errors when it fails", async () => {
+            validator.validate.returns(false);
+            validator.errors.returns(["1st error", "2nd error"]);
+
+            const results = await EventService.process(nucleusEvent, validator, inMemoryRepository);
+
+            expect(results).toHaveProperty("message", "The provided document is not valid");
+            expect(results).toHaveProperty("errors", ["1st error", "2nd error"]);
+        });
+
+        it("wraps any uncontrolled error", async () => {
+            inMemoryRepository.store.throws(new Error("Unexpected error message"));
+
+            const results = await EventService.process(nucleusEvent, validator, inMemoryRepository);
+
+            expect(results).toHaveProperty(
+                "message",
+                "There was an unexpected error while processing the event",
+            );
+            expect(results).toHaveProperty("errors", ["Unexpected error message"]);
+        });
+    });
+});
---
+++ src/functions/process-event/event-service.ts
@@ -0,0 +1,33 @@
+/**
+ * event-service.ts: Receives a collected event, parses it to convert it to an internal Nucleus
+ * event representation, then puts it into the repository.
+ */
+
+import get from "lodash/fp/get";
+import logger from "../../logger";
+
+export default class EventService {
+    public static async process(event: object, validator: Validator, repository: Repository) {
+        try {
+            logger.debug("Processing event: %j", event);
+            if (validator.validate(JSON.parse(get("content")(event)))) {
+                const record = await repository.store(event);
+
+                logger.info("Event has been processed, returning Kinesis record: %j", record);
+
+                return {message: "Event has been processed successfully"};
+            } else {
+                logger.debug("Failed event validation with errors: %j", validator.errors());
+
+                return {errors: validator.errors(), message: "The provided document is not valid"};
+            }
+        } catch (error) {
+            logger.error("Unexpected error while processing: %j", error.stack);
+
+            return {
+                errors: [error.message],
+                message: "There was an unexpected error while processing the event",
+            };
+        }
+    }
+}
