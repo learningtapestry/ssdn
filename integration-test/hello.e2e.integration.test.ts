fe64bca5988e5d9bef31aeb69c1a259bc796981d
Abraham Sánchez <abraham@twintags.com>
2019-03-25 19:23:21 +0100

    Read endpoint from the CloudFormation stack

--- integration-test/hello.e2e.integration.test.ts
+++ integration-test/hello.e2e.integration.test.ts
@@ -1,11 +1,13 @@
 "use strict";
 
 import axios from "axios";
+import { currentStack, getOutputValue } from "../test-support/aws";
 
 describe("Hello Nucleus API", () => {
   it("returns the message", async () => {
-    const url = process.env.NUCLEUS_HELLO_API || "http://localhost:3000/hello";
-    const response = await axios(url);
+    const helloNucleusEndpoint = await getOutputValue("HelloNucleusApi", currentStack());
+
+    const response = await axios(helloNucleusEndpoint);
 
     expect(response.status).toEqual(200);
     expect(response.data.message).toEqual("Hello Nucleus!");
