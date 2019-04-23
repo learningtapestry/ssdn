e0146e84bc20ca69252ac0638b0edce69abaf0e5
Abraham Sánchez <abraham@twintags.com>
2019-04-23 12:08:05 +0200

    Check valid credentials are used for AWS requests
    This works by wrapping all AWS requests in a helper function that refreshes the credentials whenever it detects an error.

--- packages/admin/src/services/aws-service.ts
+++ packages/admin/src/services/aws-service.ts
@@ -18,10 +18,7 @@ import AWSAdapter from "./aws-adapter";
 export default class AWSService {
   public static async configure() {
     Amplify.configure(awsmobile);
-    AWS.config.update({
-      credentials: await Auth.currentCredentials(),
-      region: awsmobile.aws_project_region,
-    });
+    await AWSService.updateCredentials();
     AWS.config.apiVersions = {
       cloudformation: "2010-05-15",
       cloudwatchlogs: "2014-03-28",
@@ -30,109 +27,130 @@ export default class AWSService {
     };
   }
 
-  public static async retrieveConnectionRequests({
-    documentClient = new DynamoDB.DocumentClient(),
-    type = "consumer",
-  } = {}) {
-    const params = {
-      ExpressionAttributeNames: { "#type": "type" },
-      ExpressionAttributeValues: { ":type": type },
-      FilterExpression: "#type = :type",
-      TableName: awsmobile.aws_dynamodb_table_schemas[0].tableName,
-    };
-    const connectionRequests = await documentClient.scan(params).promise();
-
-    return connectionRequests.Items as ConnectionRequest[];
+  public static async updateCredentials() {
+    AWS.config.update({
+      credentials: await Auth.currentCredentials(),
+      region: awsmobile.aws_project_region,
+    });
   }
 
-  public static async saveConnectionRequest(
-    connectionRequest: ConnectionRequest,
-    { documentClient = new DynamoDB.DocumentClient() } = {},
-  ) {
-    const params = {
-      Item: connectionRequest,
-      TableName: awsmobile.aws_dynamodb_table_schemas[0].tableName,
-    };
-    return await documentClient.put(params).promise();
+  public static async retrieveConnectionRequests({ type = "consumer" } = {}) {
+    return AWSService.withCredentials(async () => {
+      const documentClient = new DynamoDB.DocumentClient();
+      const params = {
+        ExpressionAttributeNames: { "#type": "type" },
+        ExpressionAttributeValues: { ":type": type },
+        FilterExpression: "#type = :type",
+        TableName: awsmobile.aws_dynamodb_table_schemas[0].tableName,
+      };
+      const connectionRequests = await documentClient.scan(params).promise();
+
+      return connectionRequests.Items as ConnectionRequest[];
+    });
   }
 
-  public static async availableStacks({ cloudFormation = new CloudFormation() } = {}) {
-    const stackData = await cloudFormation.describeStacks().promise();
-
-    if (stackData.Stacks) {
-      const isNucleusStack = (stack: CloudFormation.Stack) =>
-        stack.StackName.toLowerCase().startsWith("nucleus");
-
-      return AWSAdapter.convertStacks(filter(isNucleusStack)(stackData.Stacks));
-    }
+  public static async saveConnectionRequest(connectionRequest: ConnectionRequest) {
+    return AWSService.withCredentials(async () => {
+      const documentClient = new DynamoDB.DocumentClient();
+      const params = {
+        Item: connectionRequest,
+        TableName: awsmobile.aws_dynamodb_table_schemas[0].tableName,
+      };
+      return await documentClient.put(params).promise();
+    });
   }
 
-  public static async retrieveLogGroups({ cloudWatchLogs = new CloudWatchLogs() } = {}) {
-    const logGroupsData = await cloudWatchLogs
-      .describeLogGroups({ logGroupNamePrefix: "/aws/lambda/Nucleus" })
-      .promise();
+  public static async availableStacks() {
+    return AWSService.withCredentials(async () => {
+      const cloudFormation = new CloudFormation();
+      const stackData = await cloudFormation.describeStacks().promise();
 
-    return map("logGroupName")(logGroupsData.logGroups);
+      if (stackData.Stacks) {
+        const isNucleusStack = (stack: CloudFormation.Stack) =>
+          stack.StackName.toLowerCase().startsWith("nucleus");
+
+        return AWSAdapter.convertStacks(filter(isNucleusStack)(stackData.Stacks));
+      }
+    });
   }
 
-  public static async retrieveLogEvents(
-    logGroup: string,
-    { cloudWatchLogs = new CloudWatchLogs() } = {},
-  ) {
-    const streamsData = await cloudWatchLogs
-      .describeLogStreams({ logGroupName: logGroup, orderBy: "LastEventTime", descending: true })
-      .promise();
-    const eventsData = await cloudWatchLogs
-      .getLogEvents({
-        logGroupName: logGroup,
-        logStreamName: streamsData.logStreams![0].logStreamName!,
-        startFromHead: true,
-      })
-      .promise();
+  public static async retrieveLogGroups() {
+    return AWSService.withCredentials(async () => {
+      const cloudWatchLogs = new CloudWatchLogs();
+      const logGroupsData = await cloudWatchLogs
+        .describeLogGroups({ logGroupNamePrefix: "/aws/lambda/Nucleus" })
+        .promise();
 
-    return eventsData.events ? AWSAdapter.convertLogEvents(eventsData.events) : [];
+      return map("logGroupName")(logGroupsData.logGroups);
+    });
   }
 
-  public static async retrieveUsers({
-    cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider(),
-  } = {}) {
-    const userData = await cognitoIdentityServiceProvider
-      .listUsers({ UserPoolId: awsmobile.aws_user_pools_id })
-      .promise();
+  public static async retrieveLogEvents(logGroup: string) {
+    return AWSService.withCredentials(async () => {
+      const cloudWatchLogs = new CloudWatchLogs();
+      const streamsData = await cloudWatchLogs
+        .describeLogStreams({ logGroupName: logGroup, orderBy: "LastEventTime", descending: true })
+        .promise();
+      const eventsData = await cloudWatchLogs
+        .getLogEvents({
+          logGroupName: logGroup,
+          logStreamName: streamsData.logStreams![0].logStreamName!,
+          startFromHead: true,
+        })
+        .promise();
 
-    return userData.Users ? AWSAdapter.convertUsers(userData.Users) : [];
+      return eventsData.events ? AWSAdapter.convertLogEvents(eventsData.events) : [];
+    });
   }
 
-  public static async createUser(
-    userParams: UserForm,
-    { cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider() } = {},
-  ) {
-    return await cognitoIdentityServiceProvider
-      .adminCreateUser({
-        DesiredDeliveryMediums: ["EMAIL"],
-        TemporaryPassword: userParams.password,
-        UserAttributes: [
-          { Name: "email", Value: userParams.email },
-          { Name: "name", Value: userParams.name },
-          { Name: "phone_number", Value: userParams.phoneNumber },
-          { Name: "email_verified", Value: "true" },
-          { Name: "phone_number_verified", Value: "false" },
-        ],
-        UserPoolId: awsmobile.aws_user_pools_id,
-        Username: userParams.username,
-      })
-      .promise();
+  public static async retrieveUsers() {
+    return AWSService.withCredentials(async () => {
+      const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
+      const userData = await cognitoIdentityServiceProvider
+        .listUsers({ UserPoolId: awsmobile.aws_user_pools_id })
+        .promise();
+
+      return userData.Users ? AWSAdapter.convertUsers(userData.Users) : [];
+    });
   }
 
-  public static async deleteUser(
-    username: string,
-    { cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider() } = {},
-  ) {
+  public static async createUser(userParams: UserForm) {
+    return AWSService.withCredentials(async () => {
+      const cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider();
+      return await cognitoIdentityServiceProvider
+        .adminCreateUser({
+          DesiredDeliveryMediums: ["EMAIL"],
+          TemporaryPassword: userParams.password,
+          UserAttributes: [
+            { Name: "email", Value: userParams.email },
+            { Name: "name", Value: userParams.name },
+            { Name: "phone_number", Value: userParams.phoneNumber },
+            { Name: "email_verified", Value: "true" },
+            { Name: "phone_number_verified", Value: "false" },
+          ],
+          UserPoolId: awsmobile.aws_user_pools_id,
+          Username: userParams.username,
+        })
+        .promise();
+    });
+  }
+
+  public static async deleteUser(username: string) {
+    const cognitoIdentityServiceProvider = new CognitoIdentityServiceProvider();
     return await cognitoIdentityServiceProvider
       .adminDeleteUser({
         UserPoolId: awsmobile.aws_user_pools_id,
         Username: username,
       })
       .promise();
+  }
+
+  private static async withCredentials(request: () => Promise<any>) {
+    try {
+      return await request();
+    } catch (error) {
+      await AWSService.updateCredentials();
+      return await request();
+    }
   }
 }
