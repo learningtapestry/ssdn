35fb9710df03aa9bb646cf4c27ac47299ec3deeb
Rômulo Saksida <romulo@rsaksida.com>
2019-05-21 07:59:47 -0300

    Fix inconsistency in package.json
    Use NODE_ENV instead of non-standard NUCLEUS_ENV

--- packages/core/jest.config.js
+++ packages/core/jest.config.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const path = require("path");
-const currentEnv = process.env.NUCLEUS_ENV || "test";
+const currentEnv = process.env.NODE_ENV || "test";
 const pathToEnvFile = path.resolve(process.cwd(), `.env.${currentEnv}`);
 if (fs.existsSync(pathToEnvFile)) {
   require("dotenv").config({ path: pathToEnvFile });
