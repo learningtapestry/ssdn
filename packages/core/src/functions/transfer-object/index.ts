226cee34e4334dd735956234a4dc1e1974cae622
Abraham Sánchez <abraham@twintags.com>
2019-06-24 13:25:49 +0200

    Capture all transfer errors to avoid retrying
    This is a problem because it seems to retry indefinitely on error, and blocks all other transfers.

--- packages/core/src/functions/transfer-object/index.ts
+++ packages/core/src/functions/transfer-object/index.ts
@@ -1,19 +1,26 @@
 import { KinesisStreamHandler } from "aws-lambda";
 
 import Event from "../../interfaces/event";
+import logger from "../../logger";
 import { getConnectionRepository, getS3TransferService } from "../../services";
 import { parseKinesisData } from "../api-helper";
 
 const connectionRepository = getConnectionRepository();
 const s3TransferService = getS3TransferService();
 
-export const handler: KinesisStreamHandler = async (event) => {
+export const handler: KinesisStreamHandler = async (event, context, callback) => {
   for (const record of event.Records) {
     const nucleusEvent = parseKinesisData<Event>(record.kinesis.data);
     if (!nucleusEvent.source || nucleusEvent.event.protocol !== "S3") {
       continue;
     }
-    const connection = await connectionRepository.get(nucleusEvent.source.endpoint);
-    await s3TransferService.transferObject(connection, nucleusEvent);
+    try {
+      const connection = await connectionRepository.get(nucleusEvent.source.endpoint);
+      await s3TransferService.transferObject(connection, nucleusEvent);
+
+      callback(null);
+    } catch (error) {
+      logger.error(error);
+    }
   }
 };
