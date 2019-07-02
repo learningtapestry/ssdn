cb4064acac38baa697799cce6a824236b963a233
Rômulo Saksida <romulo@rsaksida.com>
2019-07-02 17:57:09 -0400

    Add more logging, including for successful actions

--- packages/core/src/functions/authorize-beacon/index.ts
+++ packages/core/src/functions/authorize-beacon/index.ts
@@ -2,6 +2,7 @@ import { CustomAuthorizerHandler } from "aws-lambda";
 import APIGateway from "aws-sdk/clients/apigateway";
 
 import TtlCache from "../../helpers/ttl-cache";
+import logger from "../../logger";
 import { getApiGatewayService } from "../../services";
 
 const cache = new TtlCache<string, APIGateway.ApiKey>();
@@ -10,6 +11,7 @@ export const handler: CustomAuthorizerHandler = async (event, _context, _callbac
   const aid = event.queryStringParameters!.aid;
 
   if (!/^[a-zA-Z0-9-_]+$/.test(aid)) {
+    logger.info(`Access denied for invalid aid ${aid}`);
     return deny("", event.methodArn);
   }
 
@@ -18,13 +20,16 @@ export const handler: CustomAuthorizerHandler = async (event, _context, _callbac
   try {
     apiGatewayKey = await cache.get(aid, () => getApiGatewayService().getApiKey({ value: aid }));
   } catch (error) {
+    logger.info(`Aid not found: ${aid}.`);
     return deny("", event.methodArn);
   }
 
   if (!apiGatewayKey.enabled) {
+    logger.info(`Aid disabled: ${aid}.`);
     return deny(apiGatewayKey.value as string, event.methodArn);
   }
 
+  logger.info(`Success for aid ${aid}.`);
   return allow(apiGatewayKey.value as string, event.methodArn);
 };
 
